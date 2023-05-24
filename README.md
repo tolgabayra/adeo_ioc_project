67fb4759329bf23c196bd839f6f0d7f2205e74b3
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-24 15:24:16 +0300

    readmefile and requiremnts added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # adeo_ioc_project
+
+
+
+#### Installion
+
+Intallion requires [Python](https://python.org/) v3.6+
+
+
+```sh
+python3 -m venv venv
+touch .env
+pip install -r requirements.txt
+python main.py
+
+```
--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
     platform = input("Select platform (1: VirusTotal, 2: UrlScan, 3: AbuseIPDB): ")
 
     if platform == "1":
-        # VirusTotal için endpoint'i ayarla
         ip.endpoint = "https://www.virustotal.com/api/v3/ip_addresses/"
         framework.add_ioc(ip)
         framework.query_all()
@@ -23,11 +22,16 @@ def main():
         framework.print_results_all()
 
     elif platform == "2":
-        # UrlScan için endpoint'i ayarla
         ip.endpoint = "https://urlscan.io/api/v1/search/?q=ip:"
+        framework.add_ioc(ip)
+        framework.query_all()
+        framework.print_results_all()
+
         domain.endpoint = "https://urlscan.io/api/v1/search/?q=domain:"
+        framework.add_ioc(domain)
+        framework.query_all()
+        framework.print_results_all()
     elif platform == "3":
-        # AbuseIPDB için endpoint'i ayarla
         ip.endpoint = "https://abuseipdb.com/api/v2/check?ipAddress="
         domain.endpoint = "https://abuseipdb.com/api/v2/check-domain?domain="
     else:
